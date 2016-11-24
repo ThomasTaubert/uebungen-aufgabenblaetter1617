d1d34a52bfcac61b84b10e6e2b98c499912f338e
nkuehnel <nkuehnel2@gmail.com>
2016-11-24 21:58:11 +0100

    Update ImmutableRational.java

--- src/aufgabenblatt3/ImmutableRational.java
+++ src/aufgabenblatt3/ImmutableRational.java
@@ -1,6 +1,11 @@
 package aufgabenblatt3;
 
 final class ImmutableRational extends Rational {
+	
+	//Nette Idee aber nicht ganz richtig ;)
+	//Die vererbte Klasse besitzt immer noch die Setter, die eine Veränderung zulassen.
+	//Außerdem sollen in dieser Klasse auch die Methoden negate und invert ein neues Rational Objekt
+	//zurückgeben.
 
 	ImmutableRational(int zaehler, int nenner) {
 		super(zaehler, nenner);
