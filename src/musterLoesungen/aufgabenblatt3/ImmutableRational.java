7cc68876fd1f3f7addd2cffbca543ee965cc271c
Thomas <Thomas@T-VAIO>
2016-12-06 16:39:19 +0100

    Pull von VSP einarbeiten

--- src/musterLoesungen/aufgabenblatt3/ImmutableRational.java
+++ src/musterLoesungen/aufgabenblatt3/ImmutableRational.java
@@ -2,19 +2,6 @@ package musterLoesungen.aufgabenblatt3;
 
 class ImmutableRational {
 
-	private int zaehler;
-	private int nenner;
-
-	public ImmutableRational() {
-		zaehler = 0;
-		nenner = 1;
-	}
-
-	public ImmutableRational(int zaehler, int nenner) {
-		this.zaehler = zaehler;
-		this.nenner = nenner;
-	}
-
 	public static void main(String[] args) {
 		ImmutableRational r = new ImmutableRational(6, 4);
 		r.printRational();
@@ -23,17 +10,8 @@ class ImmutableRational {
 		r.invert().printRational();
 		r.negate().printRational();
 	}
-
-	private void printRational() {
-		System.out.println(zaehler + "/" + nenner);
-	}
-
-=======
-package musterLösungen.aufgabenblatt3;
-
-class ImmutableRational {
-
 	private int zaehler;
+
 	private int nenner;
 
 	public ImmutableRational() {
@@ -46,36 +24,10 @@ class ImmutableRational {
 		this.nenner = nenner;
 	}
 
-	public static void main(String[] args) {
-		ImmutableRational r = new ImmutableRational(6,4);
-		r.printRational();
-		r.reduce().printRational();
-		r.add(new ImmutableRational(3, 8)).printRational();
-		r.invert().printRational();
-		r.negate().printRational();
-	}
-
-	private void printRational() {
-		System.out.println(zaehler + "/" + nenner);
-	}
-
-	
->>>>>>> branch 'master' of https://github.com/TUB-VSP-AIfI/uebungen-aufgabenblaetter1617.git
-	private ImmutableRational negate() {
-		return new ImmutableRational(this.zaehler * -1, this.nenner);
-	}
-
-	private ImmutableRational invert() {
-		return new ImmutableRational(this.nenner, this.zaehler);
-	}
-
-	private double toDouble() {
-		return (double) zaehler / nenner;
-	}
-
-	private ImmutableRational reduce() {
-		int ggT = ggt(zaehler, nenner);
-		return new ImmutableRational(zaehler / ggT, nenner / ggT);
+	private ImmutableRational add(ImmutableRational r) {
+		int zaehler = this.zaehler * r.nenner + (r.zaehler * this.nenner);
+		int nenner = this.nenner * r.nenner;
+		return new ImmutableRational(zaehler, nenner);
 	}
 
 	private int ggt(int a, int b) {
@@ -90,9 +42,24 @@ class ImmutableRational {
 		}
 	}
 
-	private ImmutableRational add(ImmutableRational r) {
-		int zaehler = this.zaehler * r.nenner + (r.zaehler * this.nenner);
-		int nenner = this.nenner * r.nenner;
-		return new ImmutableRational(zaehler, nenner);
+	private ImmutableRational invert() {
+		return new ImmutableRational(this.nenner, this.zaehler);
+	}
+
+	private ImmutableRational negate() {
+		return new ImmutableRational(this.zaehler * -1, this.nenner);
+	}
+
+	private void printRational() {
+		System.out.println(zaehler + "/" + nenner);
+	}
+
+	private ImmutableRational reduce() {
+		int ggT = ggt(zaehler, nenner);
+		return new ImmutableRational(zaehler / ggT, nenner / ggT);
+	}
+
+	private double toDouble() {
+		return (double) zaehler / nenner;
 	}
 }
