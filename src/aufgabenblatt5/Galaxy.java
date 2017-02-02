ccf5c85993dff28ae95aae40f3a204ffff4abed7
nkuehnel <nkuehnel2@gmail.com>
2017-02-02 12:27:07 +0100

    Update Galaxy.java

--- src/aufgabenblatt5/Galaxy.java
+++ src/aufgabenblatt5/Galaxy.java
@@ -21,6 +21,16 @@ class Galaxy {
 
 		for (int ii = 0; ii < 100; ii++) {
 			for (Iterator<GalaxyElement> it = galaxyElements.iterator(); it.hasNext();) {
+				
+				//das naechste Objekt eines Iterators wird ueber die Funktion
+				// next()
+				//zurueckgegeben. Es passieren beim Aufruf der Methode quasi zwei Dinge
+				//1. der "Zeiger" des Iterators schaltet eins weiter, auf das naechste Objekt
+				//2. der Iterator gibt dieses Objekt zurueck
+				//Deswegen funktioniert die for-Schleife auch ohne Inkrement, da die Weiter-
+				//schaltung in der Schleife mittels next() erfolgt
+				
+				
 //				GalaxyElement galEl = it /*
 //											 * wie bekomme ich hier das aktuelle
 //											 * Obkejt, auf das der Iterator
